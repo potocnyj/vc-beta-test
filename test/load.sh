ed44e43c72d8752f38f49ba11336975cb7a879e7
John Potocny <johnp@vividcortex.com>
2017-10-16 15:10:15 -0400

    debug

--- test/load.sh
+++ test/load.sh
@@ -1,8 +1,14 @@
 # Simple script to generate a bunch of points in the telemetry database.
 
+pwd
+
+echo "Setting up database schema..."
 mysql < test/init.sql
 
+echo "Loading data..."
 for i in `seq 1 1000`
 do
 	mysql -e "INSERT INTO telemetry.points (id, ts, value) VALUES (1, $i, FLOOR(RAND() * 1000));"
 done
+
+echo "Done."
