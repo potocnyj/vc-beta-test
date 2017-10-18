390de4e2e277e5cdb8d32aabcddf0adb8ea94221
John Potocny <johnp@vividcortex.com>
2017-10-18 10:09:33 -0400

    add some queries to our load script

--- test/load.sh
+++ test/load.sh
@@ -11,4 +11,11 @@ do
 	mysql -e "INSERT INTO telemetry.points (counter, ts, value) VALUES (1, $i, FLOOR(RAND() * 1000));"
 done
 
+# Add some new queries to the load script.
+mysql -e "SELECT * FROM telemetry.points WHERE ts > 900 LIMIT 10;"
+
+mysql -e "SELECT ts, value FROM telemetry.points;"
+
+mysql -e "SELECT value FROM telemetry.points WHERE counter = 1 AND ts = 500;"
+
 echo "Done."
